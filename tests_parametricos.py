c907db919d196b3bf5bba76ae2eaf5d8376ebeaf
adrian <adrian.canosa@gmail.com>
2014-04-03 22:15:42 +0200

    Anova una vía y POST-HOC Bonferroni.

--- tests_parametricos.py
+++ tests_parametricos.py
@@ -10,88 +10,62 @@ import scipy.stats as st
 
 def anova_test(matriz_datos, alpha):
     
-    """
-    Hipótesis (¿Dos vías?):
-    
-    Ho: No hay diferencia en las medias del factor de columna (algoritmos).
-    Ha: Al menos una media del factor de columna es diferente.
-
-    Ho: No hay diferencia en las medias de la variable de renglón (conjuntos de datos).
-    Ha: Al menos una media de la variable de renglón es diferente.
-    """
-    
     #Número de conjuntos de datos.
     N = len(matriz_datos)
     #Número de algoritmos.
     K = len(matriz_datos[0])
     
-    #Medias algoritmos (medias del factor de columna).
-    media_algoritmos = []
+    #Medias algoritmos.
+    medias_algoritmos = []
     for i in range(len(matriz_datos[0])):
-        media_algoritmos.append(sp.mean([conjunto[i] for conjunto in matriz_datos]))
+        medias_algoritmos.append(sp.mean([conjunto[i] for conjunto in matriz_datos]))
     
-    #Medias de conjuntos de datos (medias de la variable renglón).
-    media_conjunto_datos = []
-    for conjunto in matriz_datos:
-        media_conjunto_datos.append(sp.mean(conjunto))
+    #Media general.
+    media_general = sp.mean(medias_algoritmos)
     
-    #Media de medias o media total.
-    media_total = sp.mean(media_algoritmos)
-    
-    #Varianzas: total, dentro de los algoritmos, entre algoritmos.
+    #Variación total (respecto a la media general).
     SCT = 0
+    #Variación dentro del tratamiento o variación del error (cada valor respecto a la
+    #media de su tratamiento).
     SCE = 0
+    #Variación entre los diferentes tratamientos o algoritmos (efecto de la media de cada
+    #tratamiento respecto a la media general).
     SCTR = 0
     for i in range(len(matriz_datos[0])):
         x  = [conjunto[i] for conjunto in matriz_datos]
         for elem in x:
-            SCT = SCT + (elem-media_total)**2
-            SCE = SCE + (elem-media_algoritmos[i])**2
-        SCTR = SCTR + (N*(media_algoritmos[i]-media_total)**2)
-    #Varianza entre conjuntos de datos.
-    SCBL = 0
-    for j in range(N):
-        SCBL = SCBL + (K*(media_conjunto_datos[j]-media_total)**2)
+            SCT = SCT + (elem-media_general)**2
+            SCE = SCE + (elem-medias_algoritmos[i])**2
+        SCTR = SCTR + (len(x)*(medias_algoritmos[i]-media_general)**2)
         
     #Grados de libertad.
     GLT = (N*K)-1
     GLTR = K-1
-    GLBL = N-1
     GLE = GLT - GLTR
         
     #Cuadrados medios (suma cuadrados / grados de libertad)
-    #CMT = SCT/GLT
+    CMT = SCT/GLT
     CMTR = SCTR/GLTR
     CME = SCE/GLE
-    CMBL = SCBL/GLBL
     
-    #Estadísticos (para contrastar diferencia en las medias del factor de columna TR y en
-    #en las medias de la variable de renglón BL)
-    F1 = CMTR/CME
-    F2 = CMBL/CME
+    #Estadístico (para contrastar diferencia en las medias del factor de columna TR).
+    F = CMTR/CME
     
-    #Cáculo de los p_valores.
-    p_valor_F1 = 1 - st.f.cdf(F1, GLTR, GLE)
-    p_valor_F2 = 1 - st.f.cdf(F2, GLBL, GLE)
-    print 1 - st.f.cdf(178.9, 2, 12)
+    #Cáculo del p_valor.
+    p_valor = 1 - st.f.cdf(F, GLTR, GLE)
     
-    #Resultados.
-    resultados = [str(p_valor_F1 < alpha), str(p_valor_F2 < alpha)]
-    
-    return {"resultados" : resultados, "p_valores" : [p_valor_F1, p_valor_F2], "estadisticos" : [F1,F2],
-            "cuadrados_medios" : [CMTR,CMBL], "cuadrado_medio_error" : CME, "medias_algoritmos" : media_algoritmos,
-            "medias_conjuntos" : media_conjunto_datos, "media_global" : media_total}
+    return {"resultado" : str(p_valor<alpha), "p_valor" : p_valor, "estadistico" : F,
+            "variaciones" : [SCT,SCTR,SCE], "grados_libertad" : [GLT,GLTR,GLE],
+            "cuadrados_medios" : [CMT,CMTR,CME], "medias_algoritmos" : medias_algoritmos,
+            "media_general" : media_general}
 
 
 
-def bonferroni_test(nombres_algoritmos, nombres_conj_datos, medias_algoritmos, cuadrado_medio_error, alpha):
+def bonferroni_test(nombres_algoritmos, N, medias_algoritmos, cuadrado_medio_error, alpha):
     
     #Número de algoritmos K.
     K = len(medias_algoritmos)
     
-    #Número de conjuntos de datos N.
-    N = len(nombres_conj_datos)
-
     #Número posible de comparaciones.
     m = (K*(K-1))/2
 
@@ -120,34 +94,28 @@ def bonferroni_test(nombres_algoritmos, nombres_conj_datos, medias_algoritmos, c
     valores_t = list(z)
     p_valores = list(p)
     
-    print comparaciones
-    print valores_t
-    print p_valores
-    
+    #Nuevo alpha.
+    alpha2 = alpha/float(m)
 
-datos = [[0.752,0.773,0.785,0.795],
-[0.727,0.748,0.724,0.713],
-[0.736,0.716,0.585,0.638],
-[0.994,0.861,0.880,0.791],
-[0.508,0.553,0.575,0.515],
-[0.535,0.536,0.513,0.471],
-[0.967,0.871,0.954,0.532],
-[0.831,0.807,0.819,0.768],
-[0.745,0.702,0.719,0.705],
-[0.709,0.572,0.669,0.607],
-[0.722,0.728,0.725,0.732],
-[0.967,0.947,0.953,0.960],
-[0.832,0.752,0.802,0.691],
-[0.998,0.992,0.482,0.910],
-[0.963,0.963,0.954,0.926],
-[0.982,0.953,0.932,0.630],
-[0.978,0.773,0.834,0.849],
-[0.854,0.787,0.841,0.779],
-[0.965,0.984,0.995,0.947],
-[0.924,0.887,0.861,0.804],
-[0.929,0.942,0.931,0.921],
-[0.837,0.643,0.602,0.554],
-[0.972,0.956,0.944,0.922],
-[0.958,0.959,0.964,0.964]]
-#print anova_test(datos, 0.05)
-bonferroni_test(["PDFC","NNEP","IS-CHC+INN","FH-GBML"],["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23","24"],[0.84937499999999977, 0.8085416666666666, 0.79345833333333315, 0.75516666666666665],0.022017531250000003,0.05)
+    #Cálculo de los resultados.
+    resultado = []
+    for i in range(m):
+        resultado.append(p_valores[i]<alpha2)
+        
+    #Cálculo de los p_valores ajustados.
+    p_valores_ajustados = []
+    for i in range(m):
+        v = m*p_valores[i]
+        p_valores_ajustados.append(min(v,1))
+        
+    #Para seralizar JSON.
+    resultado = [str(x) for x in resultado]
+    
+    return {"valores_t" : valores_t, "p_valores" : p_valores, "comparaciones" : comparaciones, "alpha" : alpha2,
+            "resultado" : resultado, "p_valores ajustados" : p_valores_ajustados}
+
+
+
+datos = [[27,21,25],[31,33,35],[42,39,39],[38,41,37],[45,46,45]]
+print anova_test(datos, 0.05)
+print bonferroni_test(["A","B","C"],5,[36.600000000000001, 36.0, 36.200000000000003],67.166666666666671,0.05)
