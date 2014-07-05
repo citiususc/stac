ec8d0cc811787c30a9f4e97c69fbfdf5be0bdd88
adrian <adrian@adrian-VirtualBox.(none)>
2014-07-05 18:55:33 +0200

    Modificación scroll automática ayuda.

--- web/js/ver_ayuda.js
+++ web/js/ver_ayuda.js
@@ -9,7 +9,7 @@ $('#accordion').on('shown.bs.collapse', function () {
   var panel = $(this).find('.in');
   
   $('html, body').animate({
-        scrollTop: panel.offset().top
-  }, 400);
+        scrollTop: panel.offset().top -90
+  }, 1000);
   
 });
