f9118e5888115be98afd08cb4beb24cb1acd08ac
ismael.rodriguez <usuario@Ubuntu14.guest.domain>
2015-04-23 13:07:06 +0200

    Closed last issues and added import as a text insead as a file

--- web/js/file_manager.js
+++ web/js/file_manager.js
@@ -2,60 +2,71 @@ $(document).ready(function() {
 	$(document).on('click', '#upload_file', function() {
 		var formData = new FormData($('#formfile')[0]);
 		var fichero = $("#file").val();
-
-        var csvfile = $("#file")[0].files[0];
-
-        Papa.parse(csvfile, {
-            header: true,
-            dynamicTyping: true,
-            complete: function(results) {
-                var data = {};
-                data["dataset"] = [];
-                var key = results.meta.fields[0];
-                if (results.data.map(function(row) { return isNaN(row[key]); }).reduce(function(prev, curr, index, array) { return prev && curr; })) {
-                    results.meta.fields = results.meta.fields.splice(1);
-                    results.data.forEach(function(row) {
-                        data["dataset"].push(row[key]);
-			delete row[key];
-                    });
-                }
-                data["names"] = results.meta.fields;
+                var text = $("#import_text").val();
                 
-                var values = {};
-                results.meta.fields.forEach(function(field) {
-                    values[field] = [];
-                });
+                if (fichero == "" && text == "") {
+                    $("#danger_file").html("<strong>You must enter either a text data or an input file.</strong>");
+                    $("#danger_file").show();
+                } else {
+                     var csvfile = ""
+                    if (fichero != "") {
+                        csvfile = $("#file")[0].files[0];
+                    } else {
+                        csvfile = text;
+                    }
 
-                try {
-                    results.data.forEach(function(row) {
-                        if ($.map(row, function(v) {return v;}).length == results.meta.fields.length) {
+                    Papa.parse(csvfile, {
+                        header: true,
+                        dynamicTyping: true,
+                        complete: function(results) {
+                            var data = {};
+                            data["dataset"] = [];
+                            var key = results.meta.fields[0];
+                            if (results.data.map(function(row) { return isNaN(row[key]); }).reduce(function(prev, curr, index, array) { return prev && curr; })) {
+                                results.meta.fields = results.meta.fields.splice(1);
+                                results.data.forEach(function(row) {
+                                    data["dataset"].push(row[key]);
+                                    delete row[key];
+                                });
+                            }
+                            data["names"] = results.meta.fields;
+                            
+                            var values = {};
                             results.meta.fields.forEach(function(field) {
-                                if (row[field] == "-" || 
-                                    row[field] == "?") 
-                                {
-                                    values[field].push(NaN);
-                                } else if (!isNaN(row[field])) {
-                                    values[field].push(row[field]);
-                                } else {
-                                    throw "Unexpected character";
-                                }
+                                values[field] = [];
                             });
+
+                            try {
+                                results.data.forEach(function(row) {
+                                    if ($.map(row, function(v) {return v;}).length == results.meta.fields.length) {
+                                        results.meta.fields.forEach(function(field) {
+                                            if (row[field] == "-" || 
+                                                row[field] == "?") 
+                                            {
+                                                values[field].push(NaN);
+                                            } else if (!isNaN(row[field])) {
+                                                values[field].push(row[field]);
+                                            } else {
+                                                throw "Unexpected character";
+                                            }
+                                        });
+                                    }
+                                });
+                                
+                                data["values"] = values;
+                                sessionStorage["data"] = JSON.stringify(data);
+                                $("#show_file").show();
+                                $('#info_file').html("Done!");
+                                                            
+                                window.location = APP_CONFIG.app_url + "/data.html";
+                            } catch (err) {
+                                $("#danger_file").html("<strong>"+ err +"</strong>");
+                                $("#danger_file").show();
+                                $('#formfile').trigger('reset');
+                            }
                         }
                     });
-                    
-                    data["values"] = values;
-                    sessionStorage["data"] = JSON.stringify(data);
-                    $("#show_file").show();
-                    $('#info_file').html("Done!");
-						
-                    window.location = APP_CONFIG.app_url + "/file.html";
-                } catch (err) {
-                    var resultado = "<p style=\"color:red\";><strong>" + err + "</strong></p>";
-                    $('#info_file').html("<br>"+resultado);
-                    $('#formfile').trigger('reset');
                 }
-            }
-        });
 	});
 	
 	if ($(document).find("#file_table").length > 0) {
