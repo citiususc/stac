6a54e10b08e16174890749fb729481e771b3dea3
Ismael Rodríguez Fernández <ismael.rodriguez@usc.es>
2015-10-22 16:44:13 +0200

    Some minor deployment bugs

--- web/js/behavior.js
+++ web/js/behavior.js
@@ -62,6 +62,25 @@ $(document).ready(function(){
             $("#export_text").val(exportTableToCSV($('table')));
         }
     });
+    
+    // Event handler that perfoms feedback about the file selected
+    $(document).on('change', '.btn-file :file', function() {
+        var input = $(this),
+            numFiles = input.get(0).files ? input.get(0).files.length : 1,
+            label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+        input.trigger('fileselect', [numFiles, label]);
+    });
+    
+    $(document).ready( function() {
+        $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
+                var input = $(this).parents('.input-group').find(':text'),
+                                log = numFiles > 1 ? numFiles + ' files selected' : label;
+                if( input.length ) {
+                    input.val(log);
+                }
+            });
+        });
+    });
 });
 
 
